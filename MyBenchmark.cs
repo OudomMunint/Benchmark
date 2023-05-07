430e0444ec388e40fe47a4c0b4a59176be547405
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-05-07 14:47:53 +1000

    Add project files.

---
+++ MyBenchmark.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    [Config(typeof(SingleThreadPerCoreConfig))]
+    public class MyBenchmark
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly SHA512 sha512 = SHA512.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public MyBenchmark()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Sha512() => sha512.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    public class SingleThreadPerCoreConfig : ManualConfig
+    {
+        public SingleThreadPerCoreConfig()
+        {
+            var job = Job.Default.WithRuntime(CoreRuntime.Core70).WithId("SingleThreadPerCore")
+                .WithJit(Jit.RyuJit).WithGcServer(false).WithGcConcurrent(false)
+                .WithEnvironmentVariable("COMPlus_EnablePreferredAffinity", "1")
+                .WithEnvironmentVariable("COMPlus_ThreadPool_ForceMinWorkerThreads", "20");
+            Add(job);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MyBenchmark>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Benchmark;
+using BenchmarkDotNet.Running;
+using System.Management;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+
+Console.WriteLine("Welcome to the best benchmark in the entire universe");
+Console.WriteLine("-----------------------------------------------------------");
+
+// GET CPU info
+// Windows specific
+using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
+{
+    foreach (var item in searcher.Get())
+    {
+        Console.WriteLine("[CPU information]");
+        Console.WriteLine("Processor Name: {0}", item["Name"]);
+        Console.WriteLine("Manufacturer: {0}", item["Manufacturer"]);
+        Console.WriteLine("Core: {0}", item["NumberOfCores"]);
+        Console.WriteLine("Threads: {0}", item["NumberOfLogicalProcessors"]);
+        Console.WriteLine("Clock Speed: {0}MHz", item["MaxClockSpeed"]);
+        Console.WriteLine("L3 Cache: {0}MB", (Convert.ToInt64(item["L3CacheSize"]) / 1024));
+        Console.WriteLine("Voltage: {0}V", item["CurrentVoltage"]);
+        Console.WriteLine("-----------------------------------------------------------");
+    }
+}
+
+// GET RAM info
+// Windows only
+using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory"))
+{
+    // GET total MEM
+    long totalCapacity = 0;
+    string manufacturer = null;
+    foreach (ManagementObject item in searcher.Get())
+    {
+        totalCapacity += Convert.ToInt64(item["Capacity"]);
+        manufacturer = item["Manufacturer"].ToString().Trim();
+    }
+
+    Console.WriteLine("[Memory Information]");
+    Console.WriteLine("Total Capacity: {0} GB", totalCapacity / (1024 * 1024 * 1024));
+    Console.WriteLine("Manufacturer: {0}", manufacturer);
+
+    // GET Capacity per stick
+    searcher.Query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
+    int slotNumber = 1;
+    foreach (ManagementObject item in searcher.Get())
+    {
+        Console.WriteLine();
+        Console.WriteLine("(Slot {0})", slotNumber++);
+        Console.WriteLine("Speed: {0} MHz", item["Speed"]);
+        Console.WriteLine("Capacity: {0} GB", Convert.ToInt64(item["Capacity"]) / (1024 * 1024 * 1024));
+    }
+    Console.WriteLine("-----------------------------------------------------------");
+}
+
+// Windows specific
+// Retrieve GPU information
+// iGPU + dGPU
+using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController"))
+{
+    foreach (var item in searcher.Get())
+    {
+        var manufacturer = item["AdapterCompatibility"].ToString();
+        var VideoMemoryType = item["VideoMemoryType"].ToString();
+        if (manufacturer.ToLower().Contains("intel") || manufacturer.ToLower().Contains("amd"))
+        {
+            Console.WriteLine("[Integrated GPU]");
+            Console.WriteLine("Name: {0}", item["Name"]);
+            Console.WriteLine("Manufacturer: {0}", manufacturer);
+            Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
+            Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+        else
+        {
+            Console.WriteLine("[Dedicated GPU]");
+            Console.WriteLine("Name: {0}", item["Name"]);
+            Console.WriteLine("Manufacturer: {0}", manufacturer);
+            Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
+            Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+    }
+}
+
+
+// Run benchmark
+BenchmarkRunner.Run<MyBenchmark>();
