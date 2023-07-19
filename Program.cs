edc24e5eac1746b0d18b31ab3bd2565093eaa97e
OudomMunint <oudommunint@gmail.com>
2023-07-19 14:02:36 +1000

    Code clean up, Remove unused usings, Using Node v18

--- Program.cs
+++ Program.cs
@@ -3,116 +3,115 @@ using Benchmark;
 using BenchmarkDotNet.Running;
 using System.Management;
 using System.Diagnostics;
-using Microsoft.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 Console.WriteLine("Welcome to the best benchmark in the entire universe");
 Console.WriteLine("-----------------------------------------------------------");
 
 if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+{
+
+    var startInfo = new ProcessStartInfo
     {
-        
-         var startInfo = new ProcessStartInfo
-        {
-            FileName = "/usr/sbin/system_profiler",
-            Arguments = " sudo SPHardwareDataType SPDisplaysDataType",
-            RedirectStandardOutput = true,
-            UseShellExecute = false
-        };
+        FileName = "/usr/sbin/system_profiler",
+        Arguments = " sudo SPHardwareDataType SPDisplaysDataType",
+        RedirectStandardOutput = true,
+        UseShellExecute = false
+    };
 
-        var process = new Process { StartInfo = startInfo };
-        process.Start();
+    var process = new Process { StartInfo = startInfo };
+    process.Start();
 
-        while (!process.StandardOutput.EndOfStream)
-        {
-            string line = process.StandardOutput.ReadLine();
-            Console.WriteLine(line);
-        }
-
-        process.WaitForExit();
-        
+    while (!process.StandardOutput.EndOfStream)
+    {
+        string line = process.StandardOutput.ReadLine();
+        Console.WriteLine(line);
     }
 
+    process.WaitForExit();
+
+}
+
 else
+{
+    // GET CPU info
+    // Windows specific
+    using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
     {
-        // GET CPU info
-        // Windows specific
-        using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
+        foreach (var item in searcher.Get())
         {
-            foreach (var item in searcher.Get())
+            Console.WriteLine("[CPU information]");
+            Console.WriteLine("Processor Name: {0}", item["Name"]);
+            Console.WriteLine("Manufacturer: {0}", item["Manufacturer"]);
+            Console.WriteLine("Core: {0}", item["NumberOfCores"]);
+            Console.WriteLine("Threads: {0}", item["NumberOfLogicalProcessors"]);
+            Console.WriteLine("Clock Speed: {0}MHz", item["MaxClockSpeed"]);
+            Console.WriteLine("L3 Cache: {0}MB", (Convert.ToInt64(item["L3CacheSize"]) / 1024));
+            Console.WriteLine("Voltage: {0}V", item["CurrentVoltage"]);
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+    }
+
+    // GET RAM info
+    // Windows only
+    using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory"))
+    {
+        // GET total MEM
+        long totalCapacity = 0;
+        string manufacturer = null;
+        foreach (ManagementObject item in searcher.Get())
+        {
+            totalCapacity += Convert.ToInt64(item["Capacity"]);
+            manufacturer = item["Manufacturer"].ToString().Trim();
+        }
+
+        Console.WriteLine("[Memory Information]");
+        Console.WriteLine("Total Capacity: {0} GB", totalCapacity / (1024 * 1024 * 1024));
+        Console.WriteLine("Manufacturer: {0}", manufacturer);
+
+        // GET Capacity per stick
+        searcher.Query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
+        int slotNumber = 1;
+        foreach (ManagementObject item in searcher.Get())
+        {
+            Console.WriteLine();
+            Console.WriteLine("(Slot {0})", slotNumber++);
+            Console.WriteLine("Speed: {0} MHz", item["Speed"]);
+            Console.WriteLine("Capacity: {0} GB", Convert.ToInt64(item["Capacity"]) / (1024 * 1024 * 1024));
+        }
+        Console.WriteLine("-----------------------------------------------------------");
+    }
+
+    // Windows specific
+    // Retrieve GPU information
+    // iGPU + dGPU
+    using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController"))
+    {
+        foreach (var item in searcher.Get())
+        {
+            var manufacturer = item["AdapterCompatibility"].ToString();
+            var VideoMemoryType = item["VideoMemoryType"].ToString();
+            if (manufacturer.ToLower().Contains("intel") || manufacturer.ToLower().Contains("amd"))
             {
-                Console.WriteLine("[CPU information]");
-                Console.WriteLine("Processor Name: {0}", item["Name"]);
-                Console.WriteLine("Manufacturer: {0}", item["Manufacturer"]);
-                Console.WriteLine("Core: {0}", item["NumberOfCores"]);
-                Console.WriteLine("Threads: {0}", item["NumberOfLogicalProcessors"]);
-                Console.WriteLine("Clock Speed: {0}MHz", item["MaxClockSpeed"]);
-                Console.WriteLine("L3 Cache: {0}MB", (Convert.ToInt64(item["L3CacheSize"]) / 1024));
-                Console.WriteLine("Voltage: {0}V", item["CurrentVoltage"]);
+                Console.WriteLine("[Integrated GPU]");
+                Console.WriteLine("Name: {0}", item["Name"]);
+                Console.WriteLine("Manufacturer: {0}", manufacturer);
+                Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
+                Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("[Dedicated GPU]");
+                Console.WriteLine("Name: {0}", item["Name"]);
+                Console.WriteLine("Manufacturer: {0}", manufacturer);
+                Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
+                Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
                 Console.WriteLine("-----------------------------------------------------------");
             }
         }
-
-        // GET RAM info
-        // Windows only
-        using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory"))
-        {
-            // GET total MEM
-            long totalCapacity = 0;
-            string manufacturer = null;
-            foreach (ManagementObject item in searcher.Get())
-            {
-                totalCapacity += Convert.ToInt64(item["Capacity"]);
-                manufacturer = item["Manufacturer"].ToString().Trim();
-            }
-
-            Console.WriteLine("[Memory Information]");
-            Console.WriteLine("Total Capacity: {0} GB", totalCapacity / (1024 * 1024 * 1024));
-            Console.WriteLine("Manufacturer: {0}", manufacturer);
-
-            // GET Capacity per stick
-            searcher.Query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
-            int slotNumber = 1;
-            foreach (ManagementObject item in searcher.Get())
-            {
-                Console.WriteLine();
-                Console.WriteLine("(Slot {0})", slotNumber++);
-                Console.WriteLine("Speed: {0} MHz", item["Speed"]);
-                Console.WriteLine("Capacity: {0} GB", Convert.ToInt64(item["Capacity"]) / (1024 * 1024 * 1024));
-            }
-            Console.WriteLine("-----------------------------------------------------------");
-        }
-
-        // Windows specific
-        // Retrieve GPU information
-        // iGPU + dGPU
-        using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController"))
-        {
-            foreach (var item in searcher.Get())
-            {
-                var manufacturer = item["AdapterCompatibility"].ToString();
-                var VideoMemoryType = item["VideoMemoryType"].ToString();
-                if (manufacturer.ToLower().Contains("intel") || manufacturer.ToLower().Contains("amd"))
-                {
-                    Console.WriteLine("[Integrated GPU]");
-                    Console.WriteLine("Name: {0}", item["Name"]);
-                    Console.WriteLine("Manufacturer: {0}", manufacturer);
-                    Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
-                    Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
-                    Console.WriteLine("-----------------------------------------------------------");
-                }
-                else
-                {
-                    Console.WriteLine("[Dedicated GPU]");
-                    Console.WriteLine("Name: {0}", item["Name"]);
-                    Console.WriteLine("Manufacturer: {0}", manufacturer);
-                    Console.WriteLine("Driver Version: {0}", item["DriverVersion"]);
-                    Console.WriteLine("VRAM: {0}MB", Convert.ToUInt32(item["AdapterRAM"]) / (1024 * 1024));
-                    Console.WriteLine("-----------------------------------------------------------");
-                }
-            }
-        }
     }
+}
 
 Console.Write("Continue to benchmark? (y/n): ");
 var input = Console.ReadLine();
