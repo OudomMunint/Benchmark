f60f948fa7202b045393331ead0cbad8f17b6bb3
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2024-07-03 16:56:21 +1000

    Added console utils UI - Feature - Progress indicator #37

---
+++ ConsoleUtils.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Threading;
+
+class Spinner : IDisposable
+{
+    private const string Sequence = @"/-\|";
+    private int counter = 0;
+    private readonly int left;
+    private readonly int top;
+    private readonly int delay;
+    private bool active;
+    private readonly Thread thread;
+
+    public Spinner(int left, int top, int delay = 100)
+    {
+        this.left = left;
+        this.top = top;
+        this.delay = delay;
+        thread = new Thread(Spin);
+    }
+
+    public void Start()
+    {
+        active = true;
+        if (!thread.IsAlive)
+            thread.Start();
+    }
+
+    public void Stop()
+    {
+        active = false;
+        Draw(' ');
+    }
+
+    private void Spin()
+    {
+        while (active)
+        {
+            Turn();
+            Thread.Sleep(delay);
+        }
+    }
+
+    private void Draw(char c)
+    {
+        Console.SetCursorPosition(left, top);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(c);
+    }
+
+    private void Turn()
+    {
+        Draw(Sequence[++counter % Sequence.Length]);
+    }
+
+    public void Dispose()
+    {
+        Stop();
+    }
+}
+class ConsoleSpinner
+{
+    private static readonly string[] SpinnerFrames = { "/", "-", "\\", "|" };
+    private static readonly TimeSpan Interval = TimeSpan.FromSeconds(0.4);
+    private static Thread? spinnerThread;
+    private static bool stopSpinner;
+
+    public static void Start()
+    {
+        stopSpinner = false;
+        spinnerThread = new Thread(Spin);
+        spinnerThread.Start();
+    }
+
+    public static void Stop()
+    {
+        stopSpinner = true;
+        spinnerThread?.Join();
+    }
+
+    private static void Spin()
+    {
+        int frameIndex = 0;
+        while (!stopSpinner)
+        {
+            Console.Write(SpinnerFrames[frameIndex]);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            frameIndex = (frameIndex + 1) % SpinnerFrames.Length;
+            Thread.Sleep(Interval);
+        }
+    }
+}
+
+class ConsoleProgressBar
+{
+    private const int ProgressBarWidth = 50;
+    private const char ProgressBarCharacter = '#';
+    private const char BackgroundCharacter = '-';
+
+    public static void DisplayProgressBar(int progressPercentage)
+    {
+        Console.CursorVisible = false;
+
+        int completedWidth = (int)Math.Round(progressPercentage / 100.0 * ProgressBarWidth);
+        int remainingWidth = ProgressBarWidth - completedWidth;
+
+        string progressBar = new string(ProgressBarCharacter, completedWidth) + new string(BackgroundCharacter, remainingWidth);
+
+        Console.Write("[{0}] {1}%", progressBar, progressPercentage);
+
+        Console.SetCursorPosition(0, Console.CursorTop);
+    }
+}
+
+class ConsoleIndeterminateBar
+{
+    private const int ProgressBarWidth = 50;
+    private const char ProgressBarCharacter = '#';
+    private const char BackgroundCharacter = '-';
+
+    public static void DisplayProgressBar()
+    {
+        Console.CursorVisible = false;
+
+        while (true)
+        {
+            for (int i = 0; i <= ProgressBarWidth; i++)
+            {
+                Console.SetCursorPosition(i, Console.CursorTop);
+                Console.Write(ProgressBarCharacter);
+                Thread.Sleep(50);
+            }
+
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(BackgroundCharacter, ProgressBarWidth + 1));
+            Console.SetCursorPosition(0, Console.CursorTop);
+        }
+    }
+}
